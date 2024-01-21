110d84424e2de66185180c324ad53fabc568054b
Mattiwos <bmattiwos@gmail.com>
2024-01-21 14:48:46 -0800

    nun

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <div>
     
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-white dark:bg-gray-900 mt-16">
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
             <div className="mr-auto place-self-center lg:col-span-7">
                 <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
