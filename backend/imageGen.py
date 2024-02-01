0bc1ba8a98a1634c16167be23ae04fd019f1e0b5
Mattiwos <44147357+Mattiwos@users.noreply.github.com>
2024-02-01 11:51:20 -0800

    Update imageGen.py

--- backend/imageGen.py
+++ backend/imageGen.py
@@ -1,26 +1,9 @@
-from diffusers import DiffusionPipeline
+from diffusers import StableDiffusionPipeline #latest version tranformers (clips)
 import torch
+from PIL import Image
 
-# load both base & refiner
-base = DiffusionPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
-)
-base.to("cuda")
-refiner = DiffusionPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-refiner-1.0",
-    text_encoder_2=base.text_encoder_2,
-    vae=base.vae,
-    torch_dtype=torch.float16,
-    use_safetensors=True,
-    variant="fp16",
-)
-refiner.to("cuda")
+#https://huggingface.co/runwayml/stable-diffusion-v1-5
 
-<<<<<<< HEAD
-# Define how many steps and what % of steps to be run on each experts (80/20) here
-n_steps = 40
-high_noise_frac = 0.8
-=======
 class imageGen:
     def __init__(self):
         self.model_id = "runwayml/stable-diffusion-v1-5"
@@ -32,20 +15,8 @@ class imageGen:
         image = self.pipe(prompt).images[0]  
         Image.show(image);
         return image;
->>>>>>> 55f5213 (t)
 
-prompt = "A majestic lion jumping from a big stone at night"
-
-# run both experts
-image = base(
-    prompt=prompt,
-    num_inference_steps=n_steps,
-    denoising_end=high_noise_frac,
-    output_type="latent",
-).images
-image = refiner(
-    prompt=prompt,
-    num_inference_steps=n_steps,
-    denoising_start=high_noise_frac,
-    image=image,
-).images[0]
+if __name__ == '__main__':
+   prompt = "horse space walk"
+   gen = imageGen();
+   gen.generate(prompt);
