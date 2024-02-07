a2fee807f080a2b0cf878c29fb62d299b98ab18f
Brian-MT-Nguyen <greencheezin@gmail.com>
2024-02-06 21:04:13 -0800

    imageEdit is works locally given a fixed img path and prompt atm, requirements changed to be able to support imageEdit

--- backend/imageEdit.py
+++ backend/imageEdit.py
@@ -1,22 +1,28 @@
-from diffusers import StableDiffusionPipeline #latest version tranformers (clips)
+from diffusers import StableDiffusionImg2ImgPipeline #latest version tranformers (clips)
 import torch
 from PIL import Image
+import io
 """
 image edit starter
 """
-class imageGen:
+class imageEdit:
     def __init__(self):
         self.model_id = "runwayml/stable-diffusion-v1-5"
-        
-        self.pipe = StableDiffusionPipeline.from_pretrained(self.model_id, torch_dtype=torch.float16,safety_checker = None)
-        # self.pipe = self.pipe.to("cuda")
+        self.pipe = StableDiffusionImg2ImgPipeline.from_pretrained(self.model_id, torch_dtype=torch.float16, safety_checker = None)
+        self.pipe = self.pipe.to("cuda")
         self.pipe.enable_model_cpu_offload()
-    def generate(self,prompt="Didn't work sorry"):
-        image = self.pipe(prompt).images[0]  
-        Image.show(image);
-        return image;
+
+    def generate(self, img, prompt="Didn't work sorry"):
+        file = open(img, "rb")
+        og_image = Image.open(file).convert("RGB")
+        og_image = og_image.resize((768, 512))
+
+        images = self.pipe(prompt=prompt, image=og_image).images
+        images[0].save("uploads/modified.jpg")
+        return images[0]
 
 if __name__ == '__main__':
-   prompt = "horse space walk"
-   gen = imageGen();
-   gen.generate(prompt);
+   img = './uploads/original.jpg'
+   prompt = "turn it into a bamboo forest"
+   gen = imageEdit()
+   gen.generate(img, prompt)
