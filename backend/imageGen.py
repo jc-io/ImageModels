fa7bc6439fd445e50553927b0887bc4774303691
Mattiwos <bmattiwos@gmail.com>
2024-02-11 21:01:01 -0800

    test

--- backend/imageGen.py
+++ backend/imageGen.py
@@ -1,36 +1,42 @@
-from diffusers import StableDiffusionPipeline #latest version tranformers (clips)
+from diffusers import StableDiffusionPipeline  # latest version transformers (clips)
 import torch
 import base64
 from PIL import Image
 
-#https://huggingface.co/runwayml/stable-diffusion-v1-5
-
-class imageGen:
+class ImageGen:
     def __init__(self):
         self.model_id = "runwayml/stable-diffusion-v1-5"
-        
-        self.pipe = StableDiffusionPipeline.from_pretrained(self.model_id, torch_dtype=torch.float16,safety_checker = None)
-        # self.pipe = self.pipe.to("cuda")
+        self.pipe = StableDiffusionPipeline.from_pretrained(self.model_id, torch_dtype=torch.float16, safety_checker=None)
         self.pipe.enable_model_cpu_offload()
-    def generate(self,prompt="Didn't work sorry"):
-        image = self.pipe(prompt).images[0]  
-        image = image.convert('RGB')
-        # Convert the image to a byte array
-        image_byte_array = image.tobytes()
 
-        # Encode the byte array to base64
-        base64_encoded_image = base64.b64encode(image_byte_array)
+    def generate(self, prompt="horse space walk"):
+        try:
+            image = self.pipe(prompt).images[0]  
+            image = image.convert('RGB')
 
-        # Convert the base64 bytes to a string (if needed)
-        base64_encoded_image_string = base64_encoded_image.decode('utf-8')
-        data_url = f"data:image/jpeg;base64,{base64_encoded_image_string}"
+            # Convert the image to a byte array
+            with Image.open(image) as img:
+                image_byte_array = img.tobytes()
 
+            # Encode the byte array to base64
+            base64_encoded_image = base64.b64encode(image_byte_array)
 
-        # Print or use the base64 encoded image string
-        # Image.show(image);
-        return data_url;
+            # Convert the base64 bytes to a string
+            base64_encoded_image_string = base64_encoded_image.decode('utf-8')
+
+            # Format the base64 string as a data URL for HTML
+            data_url = f"data:image/jpeg;base64,{base64_encoded_image_string}"
+            return data_url
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return None
 
 if __name__ == '__main__':
-   prompt = "horse space walk"
-   gen = imageGen();
-   gen.generate(prompt);
+    prompt = "horse space walk"
+    gen = ImageGen()
+    generated_image_url = gen.generate(prompt)
+    if generated_image_url:
+        print("Generated image URL:", generated_image_url)
+    else:
+        print("Failed to generate the image.")
