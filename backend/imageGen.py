47ef917e417c37c3793ac5e02a68bd357574b7d0
unknown <cuhe@ucsc.edu>
2024-02-11 18:31:12 -0800

    implemented fast and slow model classes

--- backend/imageGen.py
+++ backend/imageGen.py
@@ -1,23 +1,35 @@
 from diffusers import StableDiffusionPipeline #latest version tranformers (clips)
+from diffusers import DiffusionPipeline # slow version
 import torch
 from PIL import Image
 
 #https://huggingface.co/runwayml/stable-diffusion-v1-5
 #fast stable diff model
 
-class imageGen:
+class imageGenFast:
     def __init__(self):
         self.model_id = "runwayml/stable-diffusion-v1-5"
         
         self.pipe = StableDiffusionPipeline.from_pretrained(self.model_id, torch_dtype=torch.float16,safety_checker = None)
         # self.pipe = self.pipe.to("cuda")
-        self.pipe.enable_model_cpu_offload()
-    def generate(self,prompt="Didn't work sorry"):
+        self.pipe.enable_model_cpu_offload()  #faster option?
+    def generate(self,prompt="No prompt given"):
         image = self.pipe(prompt).images[0]  
         image.save(prompt+".png");
         return image;
 
+class imageGenSlow:
+    def __init__(self):
+        self.model_id = "stabilityai/stable-diffusion-xl-base-1.0"
+        
+        self.pipe = DiffusionPipeline.from_pretrained(self.model_id, torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+        self.pipe = self.pipe.to("cuda")
+    def generate(self,prompt="No prompt given"):
+        image = self.pipe(prompt=prompt).images[0]  
+        image.save(prompt+".png");
+        return image;
+
 if __name__ == '__main__':
-   prompt = "people working in their house"
-   gen = imageGen();
+   prompt = "people playing basketball inside"
+   gen = imageGenFast();
    gen.generate(prompt);
--- backend/main.py
+++ backend/main.py
@@ -57,7 +57,11 @@ def generate_image():
     try:
         prompt = request.form.get('prompt');
         print("Recieved prompt: " + prompt)
-        generator = imageGen();
+        #to implement: bool(slow_model) from frontend to pick between imageGenSlow() and imageGenFast()?
+        #right now set to default imageGenFast()
+        slow_model = False
+        generator = imageGenFast() if not slow_model else generator = imageGenSlow()
+        #generator = imageGen();
         image = generator.generate(prompt);
         image.save(os.path.join(app.config['GENERATED_FOLDER'],"generated_image1.jpg"))
         images = []
