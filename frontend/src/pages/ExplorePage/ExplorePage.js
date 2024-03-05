60b0c0496c37f71dac4e6c225856bc145ee8fc20
Brian-MT-Nguyen <greencheezin@gmail.com>
2024-03-04 23:38:44 -0800

    fixing issue where bg color did not scale past the initial screen height

--- frontend/src/pages/ExplorePage/ExplorePage.js
+++ frontend/src/pages/ExplorePage/ExplorePage.js
@@ -90,31 +90,24 @@ const ExplorePage = () => {
   );
 
   return (
-    <div className="bg-primary dark:bg-primary h-screen h-full py-6 sm:py-8 lg:py-12">
-      <div className="mx-auto max-w-screen-2xl px-4 md:px-8 ">
+    <div className="bg-primary dark:bg-primary min-h-screen py-6 sm:py-8 lg:py-12"> {/* Changed h-screen to min-h-screen */}
+      <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
         <div className="mb-4 flex items-center justify-between gap-8 sm:mb-8 md:mb-12">
           <div className="flex items-center gap-12">
             <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl dark:text-white">Gallery</h2>
             <p className="hidden max-w-screen-sm text-gray-500 dark:text-gray-300 md:block">
-              This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text.
+              Explore what others are creating with our models here! 
             </p>
           </div>
-          <a href="#" className="inline-block rounded-lg border bg-white dark:bg-gray-700 dark:border-none px-4 py-2 text-center text-sm font-semibold text-gray-500 dark:text-gray-200 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:px-8 md:py-3 md:text-base">
-            More
-          </a>
         </div>
-
-
-        {images && images.length > 0 && (
-          <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8 ">
-            {images.map(image => renderImageCard(image))}
-          </div>
-        )}
-
+  
+        <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8">
+          {images?.map(image => renderImageCard(image, setSelectedImage))}
+        </div>
       </div>
       <ImageModal image={selectedImage} onClose={() => setSelectedImage(null)} />
     </div>
   );
-};
+};  
 
 export default ExplorePage;
