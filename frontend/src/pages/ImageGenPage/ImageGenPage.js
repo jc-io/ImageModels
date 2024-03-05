4427b5ce0d54e6236af5f6726171d3c2f3e1fed5
Mattiwos <bmattiwos@gmail.com>
2024-03-04 21:49:53 -0800

    moved up selector

--- frontend/src/pages/ImageGenPage/ImageGenPage.js
+++ frontend/src/pages/ImageGenPage/ImageGenPage.js
@@ -81,11 +81,6 @@ const ImageGenPage = () => {
                     <br/>
                 <input htmlFor="Caption" type="text" id="default-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setPrompt(e.target.value)}/>
                 <br/>
-
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded items-center" onClick={handleGen}>
-                Generate Image
-                </button>
-                
                 <label className="block mb-2 text-lg font-medium text-gray-900 dark:text-white">
               Choose Model:
             </label>
@@ -96,8 +91,16 @@ const ImageGenPage = () => {
               <button className={`text-white font-bold py-2 px-4 rounded ${selectedModel === 'stabilityai/stable-diffusion-xl-base-1.0' ? 'bg-buttonHover' : 'bg-blue-700'}`} onClick={() => setSelectedModel('stabilityai/stable-diffusion-xl-base-1.0')}>
                 StabilityAI (High Detail)
               </button>
+              </div>
 
-            </div>
+              <br/>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded items-center" onClick={handleGen}>
+                Generate Image
+                </button>
+                
+                
+
+            
           
                 
             </div>
