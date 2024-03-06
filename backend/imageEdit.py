758b5fe88c8bef335dfb744366e4cbbc71026dad
Mattiwos <bmattiwos@gmail.com>
2024-03-05 17:48:39 -0800

    J changes needs to be tested

--- backend/imageEdit.py
+++ backend/imageEdit.py
@@ -1,27 +1,61 @@
 from diffusers import StableDiffusionImg2ImgPipeline #latest version tranformers (clips)
+from diffusers import AutoPipelineForImage2Image
 import torch
 from PIL import Image
 import base64
 import io
-"""
-image edit starter
-"""
+#import xformers
+
 class ImageEdit:
     def __init__(self):
         self.model_id = "runwayml/stable-diffusion-v1-5"
-        self.pipe = StableDiffusionImg2ImgPipeline.from_pretrained(self.model_id, torch_dtype=torch.float16, safety_checker = None)
+        self.detailed_model_id = "stabilityai/stable-diffusion-xl-base-1.0"
+        #editImageGenerate = ImageEdit(AutoPipelineForImage2Image,"runwayml/stable-diffusion-v1-5");
+
+    def preprocess(self, img):
+        try:
+            file = open(img, "rb")
+            og_image = Image.open(file).convert("RGB")
+            og_image = og_image.resize((768, 512))
+            return og_image
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return None
+
+    def generate(self, img, prompt="Didn't work sorry", strengthImg=0.8, guidance_scaleImg=7.5, stepsImg=50, negativeImg="", num_images=1):
+        #self.pipe = StableDiffusionImg2ImgPipeline.from_pretrained(self.model_id_base, torch_dtype=torch.float16, safety_checker=None)
+        model_input_img = self.preprocess(img)
+        self.pipe = AutoPipelineForImage2Image.from_pretrained(self.model_id, torch_dtype=torch.float16, variant="fp16",use_safetensors=True)
         self.pipe = self.pipe.to("cuda")
         self.pipe.enable_model_cpu_offload()
-
-    def generate(self, img, prompt="Didn't work sorry"):
-        file = open(img, "rb")
-        og_image = Image.open(file).convert("RGB")
-        og_image = og_image.resize((768, 512))
-
-        images = self.pipe(prompt=prompt, image=og_image).images
-        images[0].save("uploads/modified.jpg")
+        #self.pipe.enable_xformers_memory_efficient_attention()
+        images = self.pipe(prompt=prompt, image=model_input_img,
+                strength=strengthImg,
+                guidance_scale=guidance_scaleImg,
+                num_inference_steps=stepsImg,
+                negative_prompt=negativeImg,
+                num_images_per_prompt=num_images
+            ).images
+        #images[0].save("uploads/modified.jpg")
         return self.covertToimgageJpeg(images[0]);
-    
+
+    def generateDetailed(self, img, prompt="Didn't work sorry", strengthImg=0.8, guidance_scaleImg=7.5, stepsImg=50, negativeImg="", num_images=1):
+        #self.pipe = StableDiffusionImg2ImgPipeline.from_pretrained(self.model_id_base, torch_dtype=torch.float16, safety_checker=None)
+        model_input_img = self.preprocess(img)
+        self.pipe = AutoPipelineForImage2Image.from_pretrained(self.model_id_detailed, torch_dtype=torch.float16, variant="fp16",use_safetensors=True)
+        self.pipe = self.pipe.to("cuda")
+        self.pipe.enable_model_cpu_offload()
+        #self.pipe.enable_xformers_memory_efficient_attention()
+        images = self.pipe(prompt=prompt, image=model_input_img,
+                strength=strengthImg,
+                guidance_scale=guidance_scaleImg,
+                num_inference_steps=stepsImg,
+                negative_prompt=negativeImg,
+                num_images_per_prompt=num_images
+            ).images
+        #images[0].save("uploads/modified.jpg")
+        return self.covertToimgageJpeg(images[0]);
+
     def covertToimgageJpeg(self, image):
         image = image.convert('RGB')
                         # Convert the image to a byte array
@@ -41,9 +75,3 @@ class ImageEdit:
             # Format the base64 string as a data URL for HTML
         data_url = f"data:image/jpeg;base64,{base64_encoded_image_string}"
         return data_url
-
-if __name__ == '__main__':
-   img = './uploads/original.jpg'
-   prompt = "turn it into a bamboo forest"
-   gen = imageEdit()
-   gen.generate(img, prompt)
