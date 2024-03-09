1d9f298e46963fa7ab11756b270d6467b84d8f3f
mattiwos <bmattiwos@gmail.com>
2024-03-08 21:10:38 -0800

    upated description of different features avaible on the website

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,47 +1,3 @@
-// First section with GIF background
-// <section className="bg-white dark:bg-fifth mt-16">
-
-// <div className="absolute w-full h-full bg-cover bg-center" style={{ backgroundImage: "url('https://miro.medium.com/v2/resize:fit:1080/format:webp/1*jcccm053U2EO63bwb55czw.gif')" }}>
-//   <div className="absolute inset-0 bg-black opacity-25"></div>
-//   {/* Content inside the first section */}
-//       <div className="absolute inset-0 flex justify-center items-center text-white z-10">
-//       <h1 className="text-4xl font-bold dark:text-white">Welcome to ImageGen</h1>
-//    </div>
-//   </div>
-//   </section>
-
-// src/pages/AboutPage.js
-// src/pages/AboutPage.js
-// {/* <section className="relative w-full h-full bg-cover bg-center" style={{ backgroundImage: "url('https://miro.medium.com/v2/resize:fit:1080/format:webp/1*jcccm053U2EO63bwb55czw.gif')" }}>
-// <video className="absolute inset-0 object-cover w-full h-full" autoPlay loop muted playsInline>
-//   <source src="https://openaicomproductionae4b.blob.core.windows.net/production-twill-01/c74791d0-75d2-48e6-acae-96d13bc97c56/paper-planes.mp4" type="video/mp4" />
-//   Your browser does not support the video tag.
-// </video>
-// <div className="flex flex-col items-center justify-center h-screen text-white z-10">
-//   <div className="text-center">
-//     <h1 className="text-4xl font-bold dark:text-white">Welcome to ImageGen</h1>
-//   </div>
-//   <div className="mt-4">
-//     <button className="px-6 py-3 bg-white text-gray-800 rounded-lg font-semibold hover:bg-gray-200">Get Started</button>
-//     {/* Alternatively, you can replace "Get Started" with "Login" */}
-//   </div>
-// </div>
-// </section> */}
-// {/* <section className="relative w-full h-full bg-cover bg-center" style={{ backgroundImage: "url('https://miro.medium.com/v2/resize:fit:1080/format:webp/1*jcccm053U2EO63bwb55czw.gif')" }}>
-// <video className="absolute inset-0 object-cover w-full h-full" autoPlay loop muted playsInline>
-//   <source src="https://openaicomproductionae4b.blob.core.windows.net/production-twill-01/c74791d0-75d2-48e6-acae-96d13bc97c56/paper-planes.mp4" type="video/mp4" />
-//   Your browser does not support the video tag.
-// </video>
-// <div className="absolute inset-0 flex flex-col justify-center items-center text-white z-10">
-//   <div className="text-center">
-//     <h1 className="text-4xl font-bold dark:text-white">Welcome to ImageGen</h1>
-//   </div>
-//   <div className="mt-4">
-//     <button className="px-6 py-3 bg-white text-gray-800 rounded-lg font-semibold hover:bg-gray-200">Get Started</button>
-//     {/* Alternatively, you can replace "Get Started" with "Login" */}
-//   </div>
-// </div>
-// </section> */}
 import React from 'react';
 
 const HomePage = () => {
@@ -74,9 +30,8 @@ const HomePage = () => {
                 <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
                     Edit Image w/ AI</h1>
                 <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl">
-                "Edit Image w/ AI" feature harnesses the power of Pix2Pix and other cutting-edge ML models from Hugging Face. These models excel in image-to-image translation tasks, enabling your users to effortlessly enhance and modify their images through a seamless and intuitive interface on your website.</p>
-                <p className='ax-w-2xl mb-6 font-lightlg:mb-8 md:text-lg lg:text-xl '>
-                    - ChatGPT</p>
+                "Edit Image w/ AI" feature harnesses the power of Stable Diffusion and other cutting-edge ML models from Hugging Face. These models excel in image-to-image translation tasks, enabling you to effortlessly enhance and modify their images through a seamless and intuitive interface on our website.</p>
+               
                 
                 
                 <a href="/EditImage" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
@@ -100,11 +55,9 @@ const HomePage = () => {
                 <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
                     Image Generator using Generative AI</h1>
                 <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl">
-                "Image Generator using Generative AI" feature leverages Stable Diffusion and Generative AI to empower users to create high-quality, diverse, and visually appealing images. The controlled diffusion process ensures stability, while the generative capabilities provide users with a dynamic and creative tool for image generation.
+                "Image Generator using Generative AI" feature leverages Stable Diffusion and Generative AI to empower users to create high-quality, diverse, and visually appealing images. The controlled diffusion process ensures stability, while the generative capabilities provide you with a dynamic and creative tool for image generation.
                 </p>
-                <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl">
-                - ChatGPT
-                </p>
+              
                 <a href="/ImageGen" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                 <button className="inline-flex items-center justify-center px-7 py-5 mr-3 text-base font-large text-center text-white bg-button hover:bg-buttonHover font-bold rounded-full">
                         Get started
@@ -130,11 +83,12 @@ const HomePage = () => {
                 <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
                     Caption Generator</h1>
                 <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl">
-                "Caption Generator" feature leverages the power of the BLIP model to provide users with automatically generated, contextually rich captions for their images. This multimodal approach ensures accurate and meaningful descriptions, enhancing the value and engagement of the visual content on your website.                    
+                "Caption Generator" feature leverages the power of the BLIP model to provide you with automatically generated, contextually rich captions for images. The next step to enhancing the caption using using a Large Language Model (LLM) is also available. This feature is perfect for generating captions for images, and it can be used in a variety of applications, including social media, content creation, and more.                    
                 </p>
                 <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl">
-                - ChatGPT
+                    Get started and generate captions for your images now! No need to comtemplate on what to write. Just upload your image and let our AI do the work for you. You CHOOSE the tone. You CHOOSE the style. You CHOOSE the length. Our AI will do the rest.
                 </p>
+             
                 
                 <a href="/CaptionGen" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                 <button className="inline-flex items-center justify-center px-7 py-5 mr-3 text-base font-large text-center text-white bg-button hover:bg-buttonHover font-bold rounded-full">
